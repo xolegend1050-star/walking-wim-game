5ed9829936033d47517e86554d93e97d5f81b68f
Xo-Dev <xolegend1050@gmail.com>
2026-09-09 04:48:44 +0530

    Fix door detection: use body_entered instead of area_entered for player CharacterBody3D

--- Door/door.gd
+++ Door/door.gd
@@ -40,13 +40,13 @@ func toggle_door():
 	target_rotation = -PI / 2.0 if is_open else 0.0
 	set_process(true)
 
-func _on_area_entered(area):
-	if area.get_parent().is_in_group("player"):
+func _on_body_entered(body):
+	if body.is_in_group("player"):
 		player_nearby = true
 		$PromptLabel.visible = true
 
-func _on_area_exited(area):
-	if area.get_parent().is_in_group("player"):
+func _on_body_exited(body):
+	if body.is_in_group("player"):
 		player_nearby = false
 		$PromptLabel.visible = false
 
