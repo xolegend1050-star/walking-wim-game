d59e7b2343193eb877bf9da7b4a9c205945c8c46
Xo-Dev <xolegend1050@gmail.com>
2026-09-09 04:25:35 +0530

    Add battery bar UI, 7 battery pickups with glow, 5 doors placed in map

--- UI/battery_ui.gd
+++ UI/battery_ui.gd
@@ -1,56 +1,47 @@
-extends Control
+extends ProgressBar
 
-var torch_bar: ProgressBar
-var label: Label
+@onready var fill_rect: ColorRect = $BarFill
+@onready var label: Label = $Label
 
 func _ready():
-	torch_bar = ProgressBar.new()
-	torch_bar.name = "TorchBar"
-	torch_bar.custom_minimum_size = Vector2(150, 20)
-	torch_bar.position = Vector2(10, 50)
-	torch_bar.max_value = 100
-	torch_bar.value = 100
-	torch_bar.visible = false
-	
-	var style = StyleBoxFlat.new()
-	style.bg_color = Color(0.2, 0.6, 0.2)
-	style.corner_radius_top_left = 4
-	style.corner_radius_top_right = 4
-	style.corner_radius_bottom_left = 4
-	style.corner_radius_bottom_right = 4
-	torch_bar.add_theme_stylebox_override("fill", style)
-	
+	# Style the progress bar
 	var bg_style = StyleBoxFlat.new()
 	bg_style.bg_color = Color(0.1, 0.1, 0.1)
 	bg_style.corner_radius_top_left = 4
 	bg_style.corner_radius_top_right = 4
 	bg_style.corner_radius_bottom_left = 4
 	bg_style.corner_radius_bottom_right = 4
-	torch_bar.add_theme_stylebox_override("background", bg_style)
+	add_theme_stylebox_override("background", bg_style)
 	
-	add_child(torch_bar)
+	var fill_style = StyleBoxFlat.new()
+	fill_style.bg_color = Color(0.2, 0.8, 0.2)
+	fill_style.corner_radius_top_left = 4
+	fill_style.corner_radius_top_right = 4
+	fill_style.corner_radius_bottom_left = 4
+	fill_style.corner_radius_bottom_right = 4
+	add_theme_stylebox_override("fill", fill_style)
 	
-	label = Label.new()
-	label.name = "BatteryLabel"
-	label.position = Vector2(10, 35)
-	label.text = "Battery"
-	label.add_theme_font_size_override("font_size", 14)
-	label.visible = false
-	add_child(label)
+	# Hide until torch is picked up
+	visible = false
 
 func _process(_delta):
 	var player = get_tree().get_first_node_in_group("player")
-	if player and player.has_torch_item:
-		torch_bar.visible = true
-		label.visible = true
-		torch_bar.value = player.torch_battery
+	if player and "has_torch_item" in player and player.has_torch_item:
+		visible = true
+		value = player.torch_battery
 		
-		if player.torch_battery > 50:
-			torch_bar.modulate = Color(0.2, 0.8, 0.2)
-		elif player.torch_battery > 20:
-			torch_bar.modulate = Color(0.8, 0.8, 0.2)
-		else:
-			torch_bar.modulate = Color(0.8, 0.2, 0.2)
+		# Update fill color based on charge level
+		var fill_style = get_theme_stylebox("fill") as StyleBoxFlat
+		if fill_style:
+			if value > 50:
+				fill_style.bg_color = Color(0.2, 0.8, 0.2)
+			elif value > 20:
+				fill_style.bg_color = Color(0.8, 0.8, 0.2)
+			else:
+				fill_style.bg_color = Color(0.8, 0.2, 0.2)
+		
+		# Update label
+		if label:
+			label.text = str(int(value)) + "%"
 	else:
-		torch_bar.visible = false
-		label.visible = false
+		visible = false
